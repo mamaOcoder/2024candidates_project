e5763e4b18a601ba2522a6e743c1e69b6164f161
Leslie <lesliekanderson@fastmail.com>
2024-06-20 14:45:50 -0400

    add updated data and 2 cleaned notebooks

--- Jupyter Notebooks/utils/scrape_functions.py
+++ Jupyter Notebooks/utils/scrape_functions.py
@@ -1,4 +1,4 @@
-# All functions used in collect_candidates.py
+# Functions to scrape and clean Candidate data
 import requests, re
 import pandas as pd
 import numpy as np
@@ -16,10 +16,9 @@ def scrape_ballotpedia_tables(url):
 
     Args:
     - url (str): The URL of the Ballotpedia page containing the table.
-    - table_order (int): The order of the table on the page (0-indexed).
 
     Returns:
-    - pd.DataFrame: The scraped table as a DataFrame.
+    - list: The scraped html tables as a list.
     """
     # Send an HTTP request to the URL and get the page content
     response = requests.get(url)
@@ -35,6 +34,16 @@ def scrape_ballotpedia_tables(url):
     return tables
 
 def get_ballotpedia_table(tables, table_order):
+    """
+    Scrape a table from a Ballotpedia page and return it as a DataFrame.
+
+    Args:
+    - tables (list): List of Ballotpedia page html tables.
+    - table_order (int): Index of the table that we want 
+
+    Returns:
+    - pd.DataFrame: The scraped table as a DataFrame.
+    """
     # Check if the specified table order exists
     if table_order >= len(tables):
         raise IndexError("Specified table order does not exist on the page")
@@ -55,6 +64,22 @@ def check_wikipedia_page(name):
         return name
     else:
         return None #"wiki page not found"
+
+def scrape_wikipedia_tables(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find all tables on the page
+    tables = soup.find_all('table')
+    
+    return tables
+
+# Function looks for wiki in href tag
+def href_wiki(href):
+    return href and re.compile("^/wiki/").search(href)
     
 def scrape_wikipedia_table(url, table_order):
     # Send a GET request to the URL
@@ -106,102 +131,3 @@ def find_state_name(s):
         return match.group(0)
     else:
         return None
-
-# Status values that means the candidate will not be on the ballot
-not_running = ['Withdrew Primary', 'Withdrew General', 'Lost Primary', 'Lost (Write-in) Primary','Withdrew (Write-in) Primary',
-               'Disqualified Primary', 'Withdrew Round 1','Disqualified General','Lost Primary Runoff','Lost (unofficially withdrew) Primary',
-               'Lost (Write-in) Primary','Withdrew Round 1','Withdrew (Write-in) General','Withdrew (unofficially withdrew) Primary',
-               'Lost Convention']
-
-def node_sizes_scaling(G,a=0,b=1,mode='lin'): # c=10,
-    if mode=='lin':
-        ns = [a + b*G.in_degree(node) for node in G.nodes()] 
-    if mode=='log':
-        ns = [a + b*np.log(G.in_degree(node)) if G.in_degree(node) > 0 else a for node in G.nodes()]
-
-    return ns
-
-def hv_plot_graph(G, node_sizes, arrowhead_length, plot_size, pos=None, nodelabels=1, node_color="green",bundled=None,
-                  partition=None, partition_colors=None, edge_color='gray',edge_line_width=1,
-                  title=None, fontsize=None,
-                  xoffset=0, yoffset=0, text_font_size=None, text_color=None, bgcolor="white"):
-    if pos is None:
-        pos = nx.spring_layout(G)
-    
-    pos_df = pd.DataFrame.from_dict(pos, orient='index', columns=['x', 'y'])
-    pos_df['index'] = pos_df.index
-    
-    if partition is not None:
-        node_colors = [partition_colors[partition[node]] for node in G.nodes()]
-    else:
-        node_colors = node_color
-    
-    plot = hvnx.draw(G, pos=pos, node_size=node_sizes, node_color=node_colors,
-                     edge_color=edge_color, edge_line_width=edge_line_width)
-
-    plot.opts(width=plot_size[0], height=plot_size[1], arrowhead_length=arrowhead_length, bgcolor=bgcolor)
-    
-    if title is not None:
-        plot = plot.opts(title=title, fontsize=fontsize.get('title', '9pt'))
-            
-    if bundled == 0:
-        if nodelabels == 1:
-            labels = hv.Labels(pos_df, ['x', 'y'], 'index')
-            plot = plot * labels.opts(xoffset=xoffset, yoffset=yoffset,
-                                      text_font_size=text_font_size, text_color=text_color,
-                                      fontsize=fontsize, bgcolor=bgcolor)
-            return plot
-        else:
-            return plot
-        
-    if bundled == 1:
-        plot = bundle_graph(plot)
-        if nodelabels==1:
-            labels = hv.Labels(pos_df, ['x', 'y'], 'index')
-            plot = plot * labels.opts(xoffset=xoffset, yoffset=yoffset,
-                                      text_font_size=text_font_size, text_color=text_color,
-                                      fontsize=fontsize, bgcolor=bgcolor)
-            return plot
-        else:
-            return plot
-
-def graph_attributes(G,nodedatalist,edgedatalist):
-    # nodedatalist/edgedatalist is a list of tuples with first element a dictionary and second element the attribute name
-    if nodedatalist!=None:
-        for (d,s) in nodedatalist:
-            for n in G.nodes():
-                G.nodes[n][s] = d[n]
-    if edgedatalist!=None:
-        for (d,s) in edgedatalist:
-            for e in G.edges():
-                G.edges[e][s] = d[e]
-
-def html_to_text_with_hyperlinks(html_file_path):
-    # Read the HTML file with a different encoding
-    with open(html_file_path, 'r', encoding='ISO-8859-1') as file:
-        html_content = file.read()
-
-    # Parse HTML using BeautifulSoup
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Extract text content
-    text_content = soup.get_text(separator='\n', strip=True)
-
-    # Extract hyperlinks and their corresponding text
-    hyperlinks = [(link.text.strip(), link.get('href')) for link in soup.find_all('a')]
-
-    # Concatenate text content with hyperlinks
-    for link_text, link_url in hyperlinks:
-        text_content += f'\n{link_text}: {link_url}'
-
-    return text_content
-
-
-def extract_urls(text):
-    # Define the regex pattern to match URLs
-    url_pattern = r'https?://\S+'
-
-    # Find all matches of the pattern in the text
-    urls = re.findall(url_pattern, text)
-
-    return urls
